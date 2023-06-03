0189649130a0682c73053b8c9b8b1d2e355d1488
Maycow Marques <melo.maycow@gmail.com>
2023-06-03 17:09:20 -0300

    initial config

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,57 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import localFont from 'next/font/local'
+import { useRouter } from 'next/router'
+import nProgress from 'nprogress'
+import { useEffect } from 'react'
+
+const inter = localFont({
+  src: [
+    {
+      path: '../../public/fonts/Inter-Regular.ttf',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: '../../public/fonts/Inter-Bold.ttf',
+      weight: '700',
+      style: 'normal',
+    },
+    {
+      path: '../../public/fonts/Inter-Black.ttf',
+      weight: '900',
+      style: 'normal',
+    },
+  ],
+  variable: '--font-inter',
+})
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const router = useRouter()
+
+  useEffect(() => {
+    const handleStart = () => {
+      nProgress.start()
+    }
+
+    const handleStop = () => {
+      nProgress.done()
+    }
+
+    router.events.on('routeChangeStart', handleStart)
+    router.events.on('routeChangeComplete', handleStop)
+    router.events.on('routeChangeError', handleStop)
+
+    return () => {
+      router.events.off('routeChangeStart', handleStart)
+      router.events.off('routeChangeComplete', handleStop)
+      router.events.off('routeChangeError', handleStop)
+    }
+  }, [router])
+
+  return (
+    <div className={`${inter.variable} font-sans`}>
+      <Component {...pageProps} />
+    </div>
+  )
 }
