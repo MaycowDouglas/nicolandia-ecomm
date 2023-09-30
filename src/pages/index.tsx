b9775401814e02f981f2968c05224760aaf39957
Maycow Marques <melo.maycow@gmail.com>
2023-09-30 06:43:34 -0300

    remove banner

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -69,14 +69,6 @@ export default function HomePage() {
           <SwiperSlide>
             <SlideImage
               alt="Diversão para toda a Família!"
-              mobile={BannerFamilySwingMobile}
-              desktop={BannerFamilySwingWeb}
-              priority
-            />
-          </SwiperSlide>
-          <SwiperSlide>
-            <SlideImage
-              alt="Diversão para toda a Família!"
               mobile={BannerBrinquedosMobile}
               desktop={BannerBrinquedosWeb}
             />
