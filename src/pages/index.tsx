3341e92924543dd63f0233565bf81b197fc23333
Maycow Marques <melo.maycow@gmail.com>
2023-10-02 17:25:41 -0300

    update banners

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,12 +7,12 @@ import SlideVideo from '@/components/atoms/SlideVideo'
 import Ticket from '@/components/molecules/Ticket'
 import Attractions from '@/components/organisms/Attractions'
 import Tickets from '@/components/organisms/Tickets'
-import BannerAntecipadoWeb from '@/public/images/banners/antecipado-desktop-promo.png'
-import BannerAntecipadoMobile from '@/public/images/banners/antecipado-mobile-promo.png'
 import BannerBrinquedosWeb from '@/public/images/banners/brinquedos-desktop.png'
 import BannerBrinquedosMobile from '@/public/images/banners/brinquedos-mobile.png'
 import BannerComboPipocaWeb from '@/public/images/banners/combo-pipoca-desktop.png'
 import BannerComboPipocaMobile from '@/public/images/banners/combo-pipoca-mobile.png'
+import BannerDiaDasCriancasWeb from '@/public/images/banners/dia-das-criancas-desktop.png'
+import BannerDiaDasCriancasMobile from '@/public/images/banners/dia-das-criancas-mobile.png'
 import BannerFamilySwingWeb from '@/public/images/banners/family-swing-desktop.png'
 import BannerFamilySwingMobile from '@/public/images/banners/family-swing-mobile.png'
 import BannerKidsWeb from '@/public/images/banners/kids-desktop.png'
@@ -66,6 +66,14 @@ export default function HomePage() {
             nextEl: '.main-swiper-next',
           }}
         >
+          <SwiperSlide>
+            <SlideImage
+              alt="Diversão para toda a Família!"
+              mobile={BannerDiaDasCriancasMobile}
+              desktop={BannerDiaDasCriancasWeb}
+            />
+          </SwiperSlide>
+
           <SwiperSlide>
             <SlideImage
               alt="Diversão para toda a Família!"
