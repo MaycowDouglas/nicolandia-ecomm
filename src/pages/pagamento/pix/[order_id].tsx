f896cec6537d9f81bb0cd90f0cfd362f59808086
Maycow Marques <melo.maycow@gmail.com>
2024-01-03 09:18:33 -0300

    add alert into pix paymento

--- src/pages/pagamento/pix/[order_id].tsx
+++ src/pages/pagamento/pix/[order_id].tsx
@@ -1,7 +1,9 @@
 /* eslint-disable @next/next/no-img-element */
 import Button from '@/components/atoms/Button'
 import { useFeedback } from '@/hooks/useFeedback'
+import PaymentPrint from '@/public/images/payment-print.png'
 import { OrderListItemResponse, OrderResponse } from '@/types/order'
+import Image from 'next/image'
 import { useRouter } from 'next/router'
 import { useEffect, useState } from 'react'
 import { FiCopy } from 'react-icons/fi'
@@ -54,7 +56,6 @@ export default function PaymentPixPage() {
                   Copiar <FiCopy />{' '}
                 </Button>
               </div>
-
               <div className="mt-5 space-y-1">
                 <p>
                   <strong>Total à pagar:</strong>{' '}
@@ -79,6 +80,20 @@ export default function PaymentPixPage() {
                     </li>
                   ))}
                 </ul>
+              </div>
+            </div>
+            <div className="max-w-4xl mx-auto bg-violet-50 p-5 rounded-lg mt-10 flex items-center gap-5 flex-col-reverse text-center md:flex-row md:text-left">
+              <div>
+                <h2 className="text-violet-800 font-bold mb-2">Atenção para pagamentos via pix</h2>
+                <p className="text-violet-800">
+                  Atenção às empresas autorizadas: ao realizar o pagamento via PIX para serviços no
+                  Parque Nicolândia, é possível que as instituições Quantico e Pagar.me sejam
+                  exibidas como opções. Ambas estão autorizadas para o recebimento de pagamentos PIX
+                  - empresas autorizadas.
+                </p>
+              </div>
+              <div className="min-w-[200px]">
+                <Image src={PaymentPrint} alt="" className="w-full h-auto" />
               </div>
             </div>
           </div>
