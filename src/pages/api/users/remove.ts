9ab4382de41b4332332b1f1af52bd30a81971fdc
Maycow Marques <melo.maycow@gmail.com>
2024-01-12 10:03:38 -0300

    fix remove user api endpoint 6

--- src/pages/api/users/remove.ts
+++ src/pages/api/users/remove.ts
@@ -5,29 +5,30 @@ import { NextApiRequest, NextApiResponse } from 'next'
 async function removeUserRoute(req: NextApiRequest, res: NextApiResponse) {
   const today = new Date()
   const { authorization } = req.headers
-
   const authorizationPayload = String(authorization).split('.')[1]
   const decodedToken = Buffer.from(authorizationPayload, 'base64')
     .toString('utf-8')
     .replaceAll('"', '')
 
-  try {
-    const user = await prisma.user_profile.findUnique({
-      where: { api_token: decodedToken },
-    })
+  res.status(200).json({ decodedToken })
 
-    if (!user) throw new Error('User not found.')
+  // try {
+  //   const user = await prisma.user_profile.findUnique({
+  //     where: { api_token: decodedToken },
+  //   })
 
-    const updated = await prisma.user_profile.update({
-      where: { api_token: decodedToken },
-      data: { email: `${user.email}.${today.getTime()}`, document: null },
-    })
+  //   if (!user) throw new Error('User not found.')
 
-    res.status(200).json({ updated })
-  } catch (error) {
-    console.error(error)
-    res.status(400).json({ error, decodedToken })
-  }
+  //   const updated = await prisma.user_profile.update({
+  //     where: { api_token: decodedToken },
+  //     data: { email: `${user.email}.${today.getTime()}`, document: null },
+  //   })
+
+  //   res.status(200).json({ updated })
+  // } catch (error) {
+  //   console.error(error)
+  //   res.status(400).json({ error, decodedToken })
+  // }
 }
 
 export default withSessionRoute(removeUserRoute)
