54bf09ccf1507385582016753389e3a67d71181c
Maycow Marques <melo.maycow@gmail.com>
2024-01-12 10:39:55 -0300

    fix remove user api endpoint 8

--- src/pages/api/users/remove.ts
+++ src/pages/api/users/remove.ts
@@ -2,32 +2,37 @@ import { prisma } from '@/lib/prisma'
 import { withSessionRoute } from '@/lib/withSession'
 import { NextApiRequest, NextApiResponse } from 'next'
 
-export default async function removeUserRoute(req: NextApiRequest, res: NextApiResponse) {
-  const today = new Date()
+export default withSessionRoute(async function GetClientRoute(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  try {
+    const { authorization } = req.headers
 
-  const { authorization } = req.headers
-  const authorizationPayload = String(authorization).split('.')[1]
-  const decodedToken = Buffer.from(authorizationPayload, 'base64')
-    .toString('utf-8')
-    .replaceAll('"', '')
+    const authorizationPayload = String(authorization).split('.')[1]
+    const decodedToken = Buffer.from(authorizationPayload, 'base64')
+      .toString('utf-8')
+      .replaceAll('"', '')
 
-  res.status(200).json({ decodedToken })
+    const transaction = await prisma.$transaction(async (tx) => {
+      const today = new Date()
+      const user = await tx.user_profile.findUnique({
+        where: { api_token: decodedToken },
+      })
 
-  // try {
-  //   const user = await prisma.user_profile.findUnique({
-  //     where: { api_token: decodedToken },
-  //   })
+      if (!user) throw new Error('User not found.')
 
-  //   if (!user) throw new Error('User not found.')
+      const updated = await tx.user_profile.update({
+        where: { api_token: decodedToken },
+        data: { email: `${user.email}.${today.getTime()}`, document: null },
+      })
 
-  //   const updated = await prisma.user_profile.update({
-  //     where: { api_token: decodedToken },
-  //     data: { email: `${user.email}.${today.getTime()}`, document: null },
-  //   })
+      return updated
+    })
 
-  //   res.status(200).json({ updated })
-  // } catch (error) {
-  //   console.error(error)
-  //   res.status(400).json({ error, decodedToken })
-  // }
-}
+    res.status(200).json({ transaction })
+  } catch (error) {
+    console.error(error)
+    res.status(400).json({ error })
+  }
+})
