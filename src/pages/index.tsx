e3e6d4aeb198710595477862f7916902376a4ec8
Maycow Marques <melo.maycow@gmail.com>
2024-02-19 15:14:14 -0300

    feat: remove carnaval banner

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -63,14 +63,6 @@ export default function HomePage() {
           <SwiperSlide>
             <SlideImage
               alt="Diversão para toda a Família!"
-              mobile={CarnavalMobile}
-              desktop={CarnavalWeb}
-            />
-          </SwiperSlide>
-
-          <SwiperSlide>
-            <SlideImage
-              alt="Diversão para toda a Família!"
               mobile={NicoCardsMobile}
               desktop={NicoCardsWeb}
             />
