e28591cc43906f306725a26db8f0e70330cf164d
Everson Araújo <eversonaraujo9@gmail.com>
2024-05-06 22:37:58 -0300

    Adição passaporte quarta/quinta maluca #2

--- src/pages/passaportes/index.tsx
+++ src/pages/passaportes/index.tsx
@@ -113,7 +113,8 @@ export default function PassaportsPage() {
                   banner={QuartaQuintaMaluca}
                   description={
                     <p className={'mt-2'}>
-                      1 passaporte válido durante 30 dias, exclusivo para utilização nas quartas e quintas-feiras, dentro do horário de funcionamento do parque."
+                      1 passaporte válido durante 30 dias, exclusivo para utilização nas quartas e
+                      quintas-feiras, dentro do horário de funcionamento do parque.
                     </p>
                   }
                 />
